352367061bf84e524925f1f42f18b890ac96ea47
Farah <91642487+FarahAbdo@users.noreply.github.com>
2022-05-13 14:38:53 +0200

    Create Simple_multiplication.py

---
+++ Simple_multiplication.py
@@ -0,0 +1,11 @@
+"""
+multiplying a given number by eight if it is an even number and by nine otherwise.
+"""
+
+#Answer 
+
+def simple_multiplication(number) :
+    if number % 2 == 0 :
+        return number * 8 
+    else :
+        return number * 9
